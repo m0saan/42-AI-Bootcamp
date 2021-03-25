d57bbfd0f0fc0b578c925e49e5832980c95b64e5
MoBoustta <moboustta6@gmail.com>
2021-03-25 21:40:44 +0100

    AI-Bootcamp module00 ex04

---
+++ module00/ex04/operations.py
@@ -0,0 +1,36 @@
+import sys
+
+
+def operations(n1: int, n2: int) -> str:
+    quotient = ""
+    remainder = ""
+
+    sum = n1 + n2
+    diff = n1 - n2
+    product = n1 * n2
+    if n2 == 0:
+        quotient = "ERROR (div by zero)"
+        remainder = "ERROR (modulo by zero)"
+    else:
+        quotient = n1 / n2
+        remainder = n1 % n2
+
+    return """    Sum:         {}
+    Difference:  {}
+    Product:     {}
+    Quotient:    {}
+    Remainder:   {}""".format(sum, diff, product, quotient, remainder)
+
+
+if __name__ == '__main__':
+    strHelp = """Example:
+    python operations.py 10 3"""
+    if len(sys.argv) == 1:
+        print(strHelp)
+    elif len(sys.argv) > 3:
+        print("InputError: too many arguments")
+    elif len(sys.argv) == 3 and type(sys.argv[1]) == str or type(sys.argv[2]) == str:
+        print("InputError: only numbers")
+        print(strHelp)
+    else:
+        print(operations(int(sys.argv[1]), int(sys.argv[2])))
