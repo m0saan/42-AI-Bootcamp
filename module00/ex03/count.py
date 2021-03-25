6d2ca0b5ec95f0909c91e6e49003150a22a05112
MoBoustta <moboustta6@gmail.com>
2021-03-25 21:40:39 +0100

    AI-Bootcamp module00 ex03

---
+++ module00/ex03/count.py
@@ -0,0 +1,32 @@
+def text_analyzer(*s) -> str:
+    """ This function counts the number of upper characters, lower characters, punctuation and spaces in a given
+    text. """
+
+    if len(s) == 0:
+        print("What is the text to analyse?")
+        s = input()
+    elif len(s) > 1:
+        print("ERROR")
+        return
+    chars_sum = 0
+    upper_letters_sum = 0
+    lower_letters_sum = 0
+    punctuation_marks_sum = 0
+    spaces_sum = 0
+
+    punctuation_marks = "!#$%&'()*+, -./:;<=>?@[\]^_`{|}~"
+
+    for char in s:
+        if char.isupper():
+            upper_letters_sum += 1
+        elif char.islower():
+            lower_letters_sum += 1
+        elif char.isspace():
+            spaces_sum += 1
+        elif char in punctuation_marks:
+            punctuation_marks_sum += 1
+        chars_sum += 1
+
+    print("The text contains {} characters: \n - {} upper letters \n - {} lower letters \n - {} punctuation marks \n -"
+          " {} spaces \n".format(chars_sum, upper_letters_sum,
+                                 lower_letters_sum, punctuation_marks_sum, spaces_sum))
