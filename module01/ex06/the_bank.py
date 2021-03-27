3d24387cc1d212f8423943ceb5c70724f2531ba2
MoBoustta <moboustta6@gmail.com>
2021-03-27 17:41:20 +0100

    AI-Bootcamp module01 ex06 -> coding ...

---
+++ module01/ex06/the_bank.py
@@ -0,0 +1,85 @@
+# in the_bank.py
+
+class Account(object):
+    ID_COUNT = 1
+
+    def __init__(self, name, **kwargs):
+        self.id = self.ID_COUNT
+        self.name = name
+        self.__dict__.update(kwargs)
+        if hasattr(self, 'value'):
+            self.value = 0
+        Account.ID_COUNT += 1
+
+    def transfer(self, amount):
+        self.value += amount
+
+
+# in the_bank.py
+
+class Bank(object):
+    """The bank"""
+
+    def __init__(self):
+        self.account = []
+
+    def add(self, account):
+        self.account.append(account)
+
+    def transfer(self, origin: Account, dest: Account, amount: float) -> bool:
+        """
+        @origin:  int(id) or str(name) of the first account
+        @dest:    int(id) or str(name) of the destination account
+        @amount:  float(amount) amount to transfer
+        @return         True if success, False if an error occurred
+        """
+
+        if not self.is_corrupted(origin) and origin in self.account and dest in self.account:
+            index_origin = self.account.index(origin)
+            index_dest = self.account.index(dest)
+            if self.account[index_origin].value >= amount:
+                self.account[index_origin].value -= amount
+                self.account[index_dest].value += amount
+                return True
+        return False
+
+    @staticmethod
+    def is_corrupted(origin):
+        ok = False
+        count = 0
+        attributes = list(filter(lambda att: not att.startswith("__"), dir(origin)))
+        if len(attributes) % 2 == 0:
+            return True
+        for attr in attributes:
+            if attr[0] == "b":
+                return True
+            if attr.startswith("zip") or attr.startswith("addr"):
+                ok = True
+            if attr == "id" or attr == "value" or attr == "name":
+                count += 1
+        return True if count != 3 or not ok else False
+
+    @staticmethod
+    def fix_account(account) -> bool:
+        """
+            fix the corrupted account
+            @account: int(id) or str(name) of the account
+            @return         True if success, False if an error occurred
+        """
+        return False
+
+
+bank = Bank()
+account1 = Account(name="Mo", addr="1250 Dr lamaaziz El Attaouia Morocco", value=1000, zip=43150)
+account2 = Account(name="Paul", addr="1250 Dr Cal USA", value=3500, zip=40000)
+account3 = Account(name="Takashi", addr="1250 Dr Tokyo Japan", value=5000, zip=20000)
+
+bank.add(account1)
+bank.add(account2)
+bank.add(account3)
+
+account1.value = 1000
+account2.value = 3500
+account3.value = 5000
+
+print(bank.transfer(account2, account1, 1500))
