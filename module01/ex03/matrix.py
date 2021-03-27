d391b87360ba1c0b6d472d69ce6bb44e1c8173fc
MoBoustta <moboustta6@gmail.com>
2021-03-27 22:56:46 +0100

    AI-Bootcamp module01 ex03 -> updated

--- module01/ex03/matrix.py
+++ module01/ex03/matrix.py
@@ -1,3 +1,5 @@
+from module01.ex02 import vector
+
 class Matrix:
     """ This is the class representation of a Matrix which is a two-dimensional collection of numbers."""
 
@@ -18,6 +20,10 @@ class Matrix:
 
     def __repr__(self):
         return '{self.__class__.__name__}(data={self.data}, size={self.shape})'.format(self=self)
+
+    def __mul__(self, other: vector):
+        """ Defining the multiplication between a matrix and a  vector."""
+        # data = [zip(other.)]
 
 
 m = Matrix((3, 3))
