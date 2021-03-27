1a6dac3d0a76616970c3d001cb846f41408e23e5
MoBoustta <moboustta6@gmail.com>
2021-03-27 17:42:02 +0100

    AI-Bootcamp module01 ex04 -> done;

--- module01/ex04/generator.py
+++ module01/ex04/generator.py
@@ -9,12 +9,15 @@ def generator(text: str, sep=" ", option=None):
         return
 
     ls_text = text.split(sep)
-    s = set()
 
     if option is not None:
         if option == "shuffle":
+            n = len(ls_text)
+            for i in range(n + 1):
+                _shuffle(i, ls_text, n)
 
         elif option == "unique":
+            s = set()
             for item in ls_text:
                 s.add(item)
             ls_text = list(s)
@@ -25,7 +28,18 @@ def generator(text: str, sep=" ", option=None):
         yield item
 
 
-txt = 1.0
+def _shuffle(i, ls_text, n):
+    change = i + randint(0, n - i)
+    if change < len(ls_text):
+        swap(ls_text, i, change)
+
+
+def swap(ls: list, i, change):
+    tmp = ls[i]
+    ls[i] = ls[change]
+    ls[change] = tmp
+
+
+txt = "Le Lorem Ipsum Ipsum du faux du texte."
 for word in generator(txt, sep=" ", option="unique"):
     print(word)
-
