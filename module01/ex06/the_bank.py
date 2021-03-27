543e5c06d7fdeb74d4323b554ec874929fe41175
MoBoustta <moboustta6@gmail.com>
2021-03-27 21:30:31 +0100

    AI-Bootcamp module01 ex06 -> start implementing the fix_account method

--- module01/ex06/the_bank.py
+++ module01/ex06/the_bank.py
@@ -34,20 +34,22 @@ class Bank(object):
         @return         True if success, False if an error occurred
         """
 
-        if not self.is_corrupted(origin) and origin in self.account and dest in self.account:
+        if not self.is_corrupted(origin) and not self.is_corrupted(
+                dest) and origin in self.account and dest in self.account:
             index_origin = self.account.index(origin)
             index_dest = self.account.index(dest)
-            if self.account[index_origin].value >= amount:
+            value = self.account[index_origin].value
+            if value >= amount and value > 0:
                 self.account[index_origin].value -= amount
                 self.account[index_dest].value += amount
                 return True
         return False
 
     @staticmethod
-    def is_corrupted(origin):
+    def is_corrupted(account):
         ok = False
         count = 0
-        attributes = list(filter(lambda att: not att.startswith("__"), dir(origin)))
+        attributes = list(filter(lambda att: not att.startswith("__"), dir(account)))
         if len(attributes) % 2 == 0:
             return True
         for attr in attributes:
@@ -59,13 +61,14 @@ class Bank(object):
                 count += 1
         return True if count != 3 or not ok else False
 
-    @staticmethod
-    def fix_account(account) -> bool:
+    def fix_account(self, account) -> bool:
         """
             fix the corrupted account
             @account: int(id) or str(name) of the account
             @return         True if success, False if an error occurred
         """
+        if self.is_corrupted(account):
+            print("YES")
         return False
 
 
@@ -73,6 +76,7 @@ bank = Bank()
 account1 = Account(name="Mo", addr="1250 Dr lamaaziz El Attaouia Morocco", value=1000, zip=43150)
 account2 = Account(name="Paul", addr="1250 Dr Cal USA", value=3500, zip=40000)
 account3 = Account(name="Takashi", addr="1250 Dr Tokyo Japan", value=5000, zip=20000)
+account4 = Account(name="Takashi", addr="1250 Dr Tokyo Japan", value=5000, zip=20000)
 
 bank.add(account1)
 bank.add(account2)
@@ -83,3 +87,4 @@ account2.value = 3500
 account3.value = 5000
 
 print(bank.transfer(account2, account1, 1500))
+print(bank.fix_account(account4))
