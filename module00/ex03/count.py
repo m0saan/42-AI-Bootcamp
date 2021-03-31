2bb5df949cbc28428914bc66be1abbf4b3eb470c
mo-san99 <moboustta6@gmail.com>
2021-03-31 15:12:05 +0100

    fix the bug -> cout not working when passing one arg`

--- module00/ex03/count.py
+++ module00/ex03/count.py
@@ -1,3 +1,5 @@
+from string import punctuation
+
 def text_analyzer(*s) -> str:
     """ This function counts the number of upper characters, lower characters, punctuation and spaces in a given
     text. """
@@ -8,13 +10,14 @@ def text_analyzer(*s) -> str:
     elif len(s) > 1:
         print("ERROR")
         return
+    elif len(s) == 1:
+        s = s[0]
+
     chars_sum = 0
     upper_letters_sum = 0
     lower_letters_sum = 0
     punctuation_marks_sum = 0
     spaces_sum = 0
-
-    punctuation_marks = "!#$%&'()*+, -./:;<=>?@[\]^_`{|}~"
 
     for char in s:
         if char.isupper():
@@ -23,10 +26,13 @@ def text_analyzer(*s) -> str:
             lower_letters_sum += 1
         elif char.isspace():
             spaces_sum += 1
-        elif char in punctuation_marks:
+        elif char in punctuation:
             punctuation_marks_sum += 1
         chars_sum += 1
 
     print("The text contains {} characters: \n - {} upper letters \n - {} lower letters \n - {} punctuation marks \n -"
           " {} spaces \n".format(chars_sum, upper_letters_sum,
                                  lower_letters_sum, punctuation_marks_sum, spaces_sum))
+
+
+text_analyzer("dfsd!!  ")
