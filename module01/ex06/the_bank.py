cceb545893244f017b4dc2d1b45a1895724ca41f
MoBoustta <moboustta6@gmail.com>
2021-04-03 00:40:10 +0100

    fix the bug -> is_corrupted not functioning well

--- module01/ex06/the_bank.py
+++ module01/ex06/the_bank.py
@@ -7,12 +7,26 @@ class Account(object):
         self.id = self.ID_COUNT
         self.name = name
         self.__dict__.update(kwargs)
-        if hasattr(self, 'value'):
-            self.value = 0
+        # if hasattr(self, 'value'):
+        #     self.value = 0
         Account.ID_COUNT += 1
 
     def transfer(self, amount):
         self.value += amount
+
+    def is_corrupted(self):
+        ok = True
+        attributes = list(self.__dict__.keys())
+        if "value" not in attributes:
+            return True
+        if len(attributes) % 2 == 0:
+            return True
+        for attr in attributes:
+            if attr[0] == "b":
+                return True
+            if attr.startswith("zip") or attr.startswith("addr"):
+                ok = False
+        return ok
 
 
 # in the_bank.py
@@ -27,48 +41,42 @@ class Bank(object):
     def add(self, account):
         self.account.append(account)
 
-    def transfer(self, origin: Account, dest: Account, amount: float) -> bool:
+    def get_account(self, p):
+        if isinstance(p, int):
+            for account in self.account:
+                if account.id == p:
+                    return account
+        if isinstance(p, str):
+            for account in self.account:
+                if account.name == p:
+                    return account
+
+    def transfer(self, origin, dest, amount: float) -> bool:
         """
         @origin:  int(id) or str(name) of the first account
         @dest:    int(id) or str(name) of the destination account
         @amount:  float(amount) amount to transfer
         @return         True if success, False if an error occurred
         """
-
-        if not self.is_corrupted(origin) and not self.is_corrupted(
-                dest) and origin in self.account and dest in self.account:
-            index_origin = self.account.index(origin)
-            index_dest = self.account.index(dest)
-            value = self.account[index_origin].value
+        origin_account = self.get_account(origin)
+        dest_account = self.get_account(dest)
+        if not isinstance(origin_account, Account) or not isinstance(dest_account, Account):
+            return False
+        elif not origin_account.is_corrupted() and not dest_account.is_corrupted():
+            value = origin_account.value
             if value >= amount and value > 0:
-                self.account[index_origin].value -= amount
-                self.account[index_dest].value += amount
+                origin_account.value -= amount
+                dest_account.transfer(amount)
                 return True
         return False
 
-    def is_corrupted(self, account):
-        ok = False
-        count = 0
-        attributes = list(filter(lambda att: not att.startswith("__"), dir(account)))
-        if len(attributes) % 2 == 0:
-            return True
-        for attr in attributes:
-            if attr[0] == "b":
-                self.has_b_prefix = True
-                return True
-            if attr.startswith("zip") or attr.startswith("addr"):
-                ok = True
-            if attr == "id" or attr == "value" or attr == "name":
-                count += 1
-        return True if count != 3 or not ok else False
-
-    def fix_account(self, account) -> bool:
+    def fix_account(self, account: Account) -> bool:
         """
             fix the corrupted account
             @account: int(id) or str(name) of the account
             @return         True if success, False if an error occurred
         """
-        if self.is_corrupted(account):
+        if account.is_corrupted():
             attr_dict = {'addr': '', 'value': 0, 'zip': 0}
             account_attrs = list(account.__dict__.keys())
             account_attrs.sort()
@@ -86,25 +94,42 @@ class Bank(object):
         return False
 
 
-bank = Bank()
-account1 = Account(name="Mo", baddr="1250 Dr lamaaziz El Attaouia Morocco", value=1000)
+if __name__ == '__main__':
+    print("\n1- Initial tow Accounts 'abdlali' and 'salmi':")
+    abdlali = Account("abdelaali", addr="el kelaa des sraghna", value=100, test=10)
+    salmi = Account("salmi", addr="bengerir", value=0, test=10)
 
-print(bank.__dict__)
-print(account1.__dict__)
+    print("\n2- Check if any accounr is corrupted:")
+    print("\tabdlali : ", abdlali.is_corrupted())
+    print("\tsalmi   : ", salmi.is_corrupted())
 
-bank.fix_account(account1)
+    print("\n3- Adding the two accounts to the Bank:")
+    bank = Bank()
+    bank.add(abdlali)
+    bank.add(salmi)
 
-# account2 = Account(name="Paul", addr="1250 Dr Cal USA", value=3500, zip=40000)
-# account3 = Account(name="Takashi", addr="1250 Dr Tokyo Japan", value=5000, zip=20000)
-# account4 = Account(name="Takashi", addr="1250 Dr Tokyo Japan", value=5000, zip=20000)
+    print("\n4- Show the amount of the two accounts:")
+    print("\tabdlali : ", abdlali.value)
+    print("\tsalmi   : ", salmi.value)
+
+    print("\n5- Make a transfer with 10 from abdlali --> salmi:")
+    bank.transfer("abdelaali", "salmi", 10)
+
+    print("\n6- Show the amount of the two accounts again:")
+    print("\tabdlali : ", abdlali.value)
+    print("\tsalmi   : ", salmi.value)
+
+    print("\n7- Create a correpted account 'mohammed' and adding it to the Bank:")
+    mohammed = Account("mohammed")
+    bank.add(mohammed)
+
+    print("\n8- Check if 'mohammed' is a corrupted account:")
+    print("\tmohammed : ", mohammed.is_corrupted())
+
+    # print("\n9- Try to fixing it:")
+    # print("\tAttributes before fixing: ", mohammed.__dict__)
+    # bank.fix_account('mohammed')
+    # print("\tAttributes after fixing: ", mohammed.__dict__)
 #
-# bank.add(account1)
-# bank.add(account2)
-# bank.add(account3)
-#
-# account1.value = 1000
-# account2.value = 3500
-# account3.value = 5000
-#
-# print(bank.transfer(account2, account1, 1500))
-# print(bank.fix_account(account4))
+# print("\n10- Check if 'mohammed' is still corrupted:")
+# print("\tmohammed : ", mohammed.is_corrupted())
