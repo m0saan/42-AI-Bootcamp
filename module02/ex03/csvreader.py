5479b46a4b42969591936c4d1cd3f0344989e64a
mo-san99 <moboustta6@gmail.com>
2021-04-03 13:57:21 +0100

    implement ex03, module02

--- module02/ex03/csvreader.py
+++ module02/ex03/csvreader.py
@@ -1,6 +1,3 @@
-from csvreader import CsvReader
-
-
 class CsvReader:
 
     def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
@@ -9,15 +6,38 @@ class CsvReader:
         self.header = header
         self.skip_top = skip_top
         self.skip_bottom = skip_bottom
+        self.fp = None  # Register the opened file.
+        self.header_line = ""
+        self.data = []
+
+    def __enter__(self):
+        self.fp = open(self.filename, "r")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.fp.close()
+
+    def print_data(self):
+        for i, line in enumerate(self.data):
+            if self.skip_top < i < len(self.data) - self.skip_bottom:
+                print(line, end="")
 
     def getdata(self):
-        pass
+        lines = self.fp.readlines()
+        n_of_elements = len(lines[1].split(self.sep))
+        if self.header:
+            self.header_line = lines[0]
+        for line in lines:
+            if len(line.split(self.sep)) != n_of_elements:
+                return None
+            self.data.append(line)
+        self.print_data()
 
     def getheader(self):
-        pass
+        return self.header_line
 
 
 if __name__ == "__main__":
-    with CsvReader('good.csv') as file:
-        data = file.getdata()
+    with CsvReader('file.csv', header=True, skip_top=10, skip_bottom=10) as file:
+        file.getdata()
         header = file.getheader()
