1210cb8383fc5fdd8b8af51f7c81d02d03a4bfc3
MoBoustta <moboustta6@gmail.com>
2021-04-03 11:17:44 +0100

    fix the bugs in supported op between matrix and scalar

--- module01/ex03/matrix.py
+++ module01/ex03/matrix.py
@@ -1,4 +1,6 @@
 from module01.ex02.vector import Vector
+import numpy as np
+
 
 def get_row(A, i: int) -> list:
     """Returns the i-th row of A (as a Vector)"""
@@ -53,23 +55,29 @@ class Matrix:
     def __add__(self, other: int):
         """ Defining the addition between a matrix and a scalar. <scalar + matrix>"""
         matrix = []
-        for ls in self.data:
-            matrix.append(list(map(lambda item: item + other, ls)))
-        return Matrix(matrix)
+        if isinstance(other, Matrix):
+            for ls1, ls2 in zip(self.data, other.data):
+                tmp_ls = [itm1 + itm2 for itm1, itm2, in zip(ls1, ls2)]
+                matrix.append(tmp_ls)
+            return Matrix(matrix)
 
     def __radd__(self, other: int):
         """ Defining the addition between a matrix and a scalar. <matrix + scalar>"""
         self + other
 
-    def __sub__(self, other: int):
+    def __sub__(self, other):
         """ Defining the subtraction between a matrix and a scalar. <scalar - matrix>"""
         matrix = []
-        if isinstance(other, Vector):
-            return self.dot(self, other)
+        if isinstance(other, Matrix):
+            for ls1, ls2 in zip(self.data, other.data):
+                tmp_ls = [itm1 - itm2 for itm1, itm2, in zip(ls1, ls2)]
+                matrix.append(tmp_ls)
+            return Matrix(matrix)
 
-        for ls in self.data:
-            matrix.append(list(map(lambda item: item - other, ls)))
-        return Matrix(matrix)
+        elif isinstance(other, int):
+            for ls in self.data:
+                matrix.append(list(map(lambda item: item - other, ls)))
+            return Matrix(matrix)
 
     def __rsub__(self, other: int):
         """ Defining the subtraction between a matrix and a scalar. <matrix - scalar>"""
@@ -80,34 +88,44 @@ class Matrix:
         matrix = []
 
         if isinstance(other, Matrix):
-            if self.shape[1] != other.shape[0]:
+            if self.shape != other.shape:
                 raise ValueError("Cannot perform multiplication")
-            ans = [[0 for x in range(other.shape[1])] for y in range(self.shape[0])]
-            n_columns = 0
-            for i, ls in enumerate(self.data):
-                _sum = 0
-                for j, v in enumerate(ls):
-                    for k, l in enumerate(get_column(other.data, n_columns)):
-                        _sum += (self.data[i][k] + other.data[k][j])
-                    ans[i][j] = _sum
-            return Matrix(ans)
+            for ls1, ls2 in zip(self.data, other.data):
+                ans = []
+                for itm1, itm2 in zip(ls1, ls2):
+                    ans.append(itm1*itm2)
+                matrix.append(ans)
+            return Matrix(matrix)
 
         elif isinstance(other, Vector):
             for ls in self.data:
-                matrix.append(list(map(lambda item: item * other, ls)))
+                ans = 0
+                for item1, item2 in zip(ls, other.values):
+                    ans += (item1 * item2)
+                matrix.append(ans)
             return Matrix(matrix)
 
         elif isinstance(other, list):
             for ls in self.data:
-                matrix.append(list(map(lambda item: item * other, ls)))
+                ans = 0
+                for item1, item2 in zip(ls, other):
+                    ans += (item1 * item2)
+                matrix.append(ans)
+            return Matrix(matrix)
+        elif isinstance(other, int):
+            for ls in self.data:
+                tmp_ls = [other * elem for elem in ls]
+                matrix.append(tmp_ls)
             return Matrix(matrix)
 
     def __rmul__(self, other):
         """ Defining the multiplication between a matrix and a scalar. <scalar * matrix>"""
-        self * other
+        return self * other
 
     def __truediv__(self, other: int):
-        """ Defining the multiplication between a matrix and a scalar. <matrix / scalar>"""
+        """ Defining the division between a matrix and a scalar. <matrix / scalar>"""
+        if not isinstance(other, int):
+            raise RuntimeError("Unsupported operation {Matrix / Matrix}.")
         if other == 0:
             raise ValueError("Cannot divide by zero")
         matrix = []
@@ -116,18 +134,21 @@ class Matrix:
         return Matrix(matrix)
 
     def __rtruediv__(self, other: int):
-        """ Defining the multiplication between a matrix and a scalar. <scalar / matrix>"""
-        self / other
+        """ Defining the division between a matrix and a scalar. <scalar / matrix>"""
+        if not isinstance(other, int):
+            raise RuntimeError("Unsupported operation {Matrix / Matrix}.")
+        matrix = []
+        for ls in self.data:
+            matrix.append(list(map(lambda item: other / item, ls)))
+        return Matrix(matrix)
 
 
 m1 = Matrix([[4, 1], [6, 3], [2, 4]])
 m2 = Matrix([[2, 5, 2, -5], [3, 5, -2, 4]])
 
-
-
 v_sum = m1 * 5
 
-#print(v_sum)
+# print(v_sum)
 
 # m2 = m1 + 2
 # m3 = m1 * 2
@@ -136,3 +157,41 @@ v_sum = m1 * 5
 # print(m3)
 # print(m4)
 # (Matrix [[28., 34.], [56., 68.]])
+
+
+A = Matrix([[4, 8], [3, 7]])
+B = Matrix([[1, 0], [5, 2]])
+
+C = Matrix([[2, 8], [0, 9]])
+D = Matrix([[5, 6], [11, 3]])
+
+E = Matrix([[5, 2], [3, 1]])
+# print(A + B)
+# print(C - D)
+# print(2 * E)
+
+m1 = Matrix([
+    [5, 1, 3],
+    [1, 1, 1],
+    [1, 2, 1]
+])
+m2 = Matrix([
+    [4, 1, 2],
+    [6, 3, 4],
+    [2, 4, 1]
+])
+
+np1 = np.array([
+    [5, 1, 3],
+    [1, 1, 1],
+    [1, 2, 1]]
+)
+
+np2 = np.array([
+    [4, 1, 2],
+    [6, 3, 4],
+    [2, 4, 1]
+])
+
+print(m1-m2)
+print(np1-np2)
