78e99fca6d87e9e4eef2d5d9177bef818d54d5dc
MoBoustta <moboustta6@gmail.com>
2021-04-03 11:19:27 +0100

    test fixed bugs

--- module01/ex03/test.py
+++ module01/ex03/test.py
@@ -1,40 +1,20 @@
-#  import unittest
-#
-#  m1 = Matrix([[0.0, 1.0, 2.0, 3.0], [0.0, 2.0, 4.0, 6.0]])
-#  m2 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0], [6.0, 7.0]])
-#  m3 = Matrix((3, 3))
-#
-#
-#  # print(m1 * m2)
-#
-#
-#  # (Matrix [[28., 34.], [56., 68.]])
-#
-#  class TestVectorInitialization(unittest.TestCase):
-#      """Testing the class vector init"""
-#
-#      def test_list_initialization1(self):
-#          self.assertEqual(m1.data, [[0.0, 1.0, 2.0, 3.0], [0.0, 2.0, 4.0, 6.0]], "Should be equal to [[0.0, 1.0, "
-#                                                                                  "2.0, 3.0], [0.0, 2.0, 4.0, 6.0]]")
-#          self.assertEqual(m1.shape, (2, 4), "should be equal (3, 2)")
-#
-#      def test_list_initialization2(self):
-#          self.assertEqual(m2.data, [[0.0, 1.0], [2.0, 3.0], [4.0, 5.0], [6.0, 7.0]], "Should be equal [[0.0, 1.0], "
-#                                                                                      "[2.0, 3.0], [4.0, 5.0], [6.0, "
-#                                                                                      "7.0]]")
-#          self.assertEqual(m2.shape, (3, 2), "should be equal to (3, 2)")
-#
-#      def test_init_matrix_with_tuple(self):
-#          self.assertEqual(m3.data, [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], "should be equal to [[0.0, 0.0, "
-#                                                                                         "0.0], [0.0, 0.0, 0.0],[0.0, "
-#                                                                                         "0.0, 0.0]]")
-#
-#
-#  if __name__ == '__main__':
-#      unittest.main()
 
 
 from matrix import Matrix
+import numpy as np
+
+A = Matrix([[4, 8], [3, 7]])
+B = Matrix([[1, 0], [5, 2]])
+
+C = Matrix([[2, 8], [0, 9]])
+D = Matrix([[5, 6], [11, 3]])
+
+E = Matrix([[5, 2], [3, 1]])
+
+
+print(A + B)
+print(C - D)
+print(2 * E)
 
 m1 = Matrix([
     [5, 1, 3],
@@ -42,15 +22,32 @@ m1 = Matrix([
     [1, 2, 1]
 ])
 m2 = Matrix([
+    [4, 1, 2],
+    [6, 3, 4],
+    [2, 4, 1]
+])
+
+np1 = np.array([
     [5, 1, 3],
     [1, 1, 1],
-    [1, 2, 1]
+    [1, 2, 1]]
+)
+
+np2 = np.array([
+    [4, 1, 2],
+    [6, 3, 4],
+    [2, 4, 1]
 ])
+
+print(m1-m2)
+print(np1-np2)
+
+
 print("\nm1 => ", repr(m1))
 print("\nm2 => ", repr(m2))
 
 print("\n---------------- Test (m1 * [1, 2, 3]) ----------------")
-print(m1 * [1, 2, 3])
+print(m1 * [2, 5])
 
 print("\n---------------- Test (m1 + m2) ----------------")
 print(m1 + m2)
