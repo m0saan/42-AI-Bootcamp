b2b0801bb0389eedd698da2804ec92030d17ec91
mo-san99 <moboustta6@gmail.com>
2021-04-04 12:17:21 +0100

    python bootcamp ex01 module03: done

--- module03/ex01/ImageProcessor.py
+++ module03/ex01/ImageProcessor.py
@@ -1,4 +1,6 @@
-import matplotlib as matplot
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 
 
 class ImageProcessor:
@@ -11,9 +13,22 @@ class ImageProcessor:
 
         if not isinstance(path, str):
             raise ValueError(f"Invalid argument type expected str got={type(path)}")
-        return None
+        im = Image.open(path)
+        width, height = im.size  # get the dimensions of the image.
+        pixels_array = np.array(im)  # Make a Numpy array
+        print("Loading image of dimensions {} x {}".format(width, height))
+        return pixels_array
+
+    def display(self, array):
+        """ Takes a NumPy array as an argument and displays the corresponding RGB image. """
+        a11 = array.reshape(array.shape)
+        plt.imshow(a11)
+        # plt.colorbar() # This would show up a color bar on the right hand side of the image
+        plt.show()
 
 
 if __name__ == '__main__':
     imp = ImageProcessor()
-    arr = imp.load("../resources/42AI.png")
+    arr = imp.load("./image.png")
+    imp.display(arr)
+    # print(arr)
