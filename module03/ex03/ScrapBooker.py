17fab4b4dac3701260472005a1176f8d3a69b3b8
MoBoustta <moboustta6@gmail.com>
2021-04-06 00:17:11 +0100

    implementing the ScrapBooker class -> add thin method

--- module03/ex03/ScrapBooker.py
+++ module03/ex03/ScrapBooker.py
@@ -20,19 +20,26 @@ class ScrapBooker:
 
     def thin(self, array: type(np.array), n: int, axis: int):
         """ deletes every n-th pixel row along the specified axis. """
+
         lst = []
         if not axis:
             for arr in array:
-                tmp = []
-                for i in range(0, len(arr), n):
-                    if i <= len(arr):
-                        tmp = np.delete(arr, i+n)
-                        arr = tmp
+                for i in range(n - 1, len(arr), n - 1):
+                    if i >= len(arr):
+                        break
+                    tmp = np.delete(arr, i)
+                    arr = tmp
                 lst.append(arr)
             return np.array(lst)
         else:
-            print("YAy.")
+            for i in range(n - 1, array.shape[0], n-1):
+                if i >= array.shape[0]:
+                    break
+                array = np.delete(array, i, 0)
+            return array
 
+    def juxtapose(self, array, n, axis):
+        """ juxtaposes n copies of the image along the specified axis (0 vertical, 1 horizontal). """
 
 if __name__ == '__main__':
     sb = ScrapBooker()
@@ -46,4 +53,19 @@ if __name__ == '__main__':
     ])
     # print(sb.crop(array=M, dimensions=(10, 3), position=(1, 1)))
     print(sb.thin(M, 3, 0))
+    print("-----------------------------------------")
 
+    N = np.array([["AAAAAAAAAAAA"],
+                  ["BBBBBBBBBBBB"],
+                  ["CCCCCCCCCCCC"],
+                  ["DDDDDDDDDDDD"],
+                  ["EEEEEEEEEEEE"],
+                  ["FFFFFFFFFFFF"],
+                  ["GGGGGGGGGGGG"],
+                  ["HHHHHHHHHHHH"],
+                  ["IIIIIIIIIIII"],
+                  ["JJJJJJJJJJJJ"],
+                  ["KKKKKKKKKKKK"],
+                  ["LLLLLLLLLLLL"]
+                  ])
+    print(sb.thin(N, 4, 1))
