eb1581430cef90c5362a35967912de82eb20d946
mo-san99 <moboustta6@gmail.com>
2021-04-06 16:01:22 +0100

    implementing the Colorfilter methods -> to_green, to_red, to_blue

--- module03/ex04/ColorFilter.py
+++ module03/ex04/ColorFilter.py
@@ -11,27 +11,59 @@ class ColorFilter:
         :return np.array:
         Takes a NumPy array of an image as an argument and returns an array with inverted color."""
 
-        lst = []
-        for i in range(0, array.shape[0]):
-            tmp_list = []
-            for j in range(0, array.shape[1]):
-                tmp_list.append(array[i][j])
-            lst.append(tmp_list)
-        return np.array(lst)
+        """ Mathematically, to invert the color of one pixel,
+        we subtract the pixel's color values from the maximum, 255."""
+        colors_arr = array[:, :, :3]  # we have an extra alpha channel that we don't want to modify, or the
+        # transparent parts of the image won't be transparent anymore.
+        # We can use numpy slice notation to modify all dimensions of the array.
+        colors_arr = 255 - colors_arr
+        return colors_arr
 
     def to_blue(self, array):
         """
         :return: np.array
         Takes a NumPy array of an image as an argument and returns an array with a blue filter.
         """
-        pass
+        array[:, :, 0] = 0
+        array[:, :, 1] = 0
+        return array
+
+    def to_green(self, array):
+        """
+        :return: np.array
+        Takes a NumPy array of an image as an argument and returns an array with a green filter.
+        """
+        array[:, :, 0] = 0
+        array[:, :, 2] = 0
+        return array
+
+    def to_red(self, array):
+        """
+        :return: np.array
+        Takes a NumPy array of an image as an argument and returns an array with a red filter.
+        """
+        array[:, :, 1] = 0
+        array[:, :, 2] = 0
+        return array
+
+    def to_red(self, array):
+        """
+        :return: np.array
+        Takes a NumPy array of an image as an argument and returns an array with a red filter.
+        """
+        array[:, :, 0] = 81
+        array[:, :, 1] = 81
+        array[:, :, 2] = 83
+        return array
 
 
 if __name__ == '__main__':
     imp = ImageProcessor()
     arr = imp.load("42AI.png")
-    print(arr)
+    # print(arr)
     cf = ColorFilter()
-    cf.invert(arr)
-    print(arr)
+    # arr = cf.invert(arr)
+    # print(arr)
+
+    arr = cf.to_(arr)
     imp.display(arr)
